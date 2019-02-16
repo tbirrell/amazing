1e3a3365c19912b00c381e9d95902bbab491b435
Timothy <tbirrel@xristos.net>
2019-02-16 03:28:38 -0600

    fixed order of checks

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -31,36 +31,36 @@ class CommentController extends Controller
     
     protected function can_comment($replying_to, $id)
     {
-        if ($replying_to === 'post') {
-            //get last comment for post
-            $last_comment = Comment::where('repliable_type', 'post')
-                                   ->where('repliable_id', $id)
-                                   ->orderBy('created_at', 'desc')
-                                   ->first();
-            
-            //if there are not other comments
-            if ($last_comment === null) {
-                return true;
-            }
-            
-            //check to see if current user posted it.
-            if ($last_comment->user_id !== auth()->user()->id) {
-                return true;
-            }
-    
-            $this->cant_comment_because = 'Consecutive comments not allowed';
-        } else if ($replying_to === 'comment') {
+        //if a reply, we have a special check
+        if ($replying_to === 'comment') {
             //get parent
             $parent = Comment::find($id);
-            
+        
             //check to see if current user is the author of parent
-            if ($parent->user_id !== auth()->user()->id) {
-                return true;
+            if ($parent->user_id === auth()->user()->id) {
+                $this->cant_comment_because = 'Replying to own comment not allowed';
+                return false;
             }
-    
-            $this->cant_comment_because = 'Replying to own comment not allowed';
+        
         }
         
+        //get last comment for post
+        $last_comment = Comment::where('repliable_type', $replying_to)
+                               ->where('repliable_id', $id)
+                               ->orderBy('created_at', 'desc')
+                               ->first();
+        
+        //if there are not other comments
+        if ($last_comment === null) {
+            return true;
+        }
+        
+        //check to see if current user posted it.
+        if ($last_comment->user_id !== auth()->user()->id) {
+            return true;
+        }
+
+        $this->cant_comment_because = 'Consecutive comments not allowed';
         return false;
     }
 }
