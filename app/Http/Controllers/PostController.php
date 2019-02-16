3de48f75e179dec04e5ecc1aae45b14dd3948d3c
Timothy <tbirrel@xristos.net>
2019-02-15 23:52:10 -0600

    controller outlines

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create(Request $request)
+    {
+    
+    }
+    
+    public function like(Request $request)
+    {
+        //if previously reacted, overwrite
+    }
+    
+    public function dislike(Request $request)
+    {
+        //if previously reacted, overwrite
+    }
+}
