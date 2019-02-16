30a004beb740a586c43310a3f0363e69f29a1e01
Timothy <tbirrel@xristos.net>
2019-02-16 01:24:29 -0600

    create comments with consecutive validation

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Comment;
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -11,11 +12,32 @@ class CommentController extends Controller
     public function create(Request $request)
     {
         $data = $request->all();
-        //todo check to sequential comments
-        Comment::create([
-            'post_id' => $data['post_id'],
-            'comment' => $data['comment'],
-            'user_id' => auth()->user()->id
-        ]);
+        
+        if ($this->can_comment($data['post_id'])) {
+            Comment::create([
+                'post_id' => $data['post_id'],
+                'comment' => $data['comment'],
+                'user_id' => auth()->user()->id
+            ]);
+            
+            return response()->json(['success' => 'Comment posted']);
+        } else {
+            return response()->json(['error' => 'Consecutive comments not allowed']);
+        }
+    }
+    
+    protected function can_comment($post_id)
+    {
+        //get last comment for post
+        $last_comment = Comment::where('post_id', $post_id)
+               ->orderBy('created_at', 'desc')
+               ->first();
+        
+        //check to see if current user posted it.
+        if ($last_comment->user_id === auth()->user()->id) {
+            return false;
+        }
+        
+        return true;
     }
 }
